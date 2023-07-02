f7198cd57a613ea9e1f593cd6fd10dc9b0632c69
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-07-02 22:36:36 +0300

    доработка кнопок удаление - сохранение

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,20 +1,15 @@
+import React, { useState} from "react";
 import { useLocation } from 'react-router-dom';
 import { convertDuration } from '../../../utils/utils';
-import MovieButton from '../MovieButton/MovieButton';
-import React from "react";
 import "./MoviesCard.css";
+import saveFilmButton from "../../../images/like-no-active.svg";
+import deleteFilmButton from "../../../images/deleteFilmButton.svg";
+import saveButton from "../../../images/like-active.svg";
 
 const MoviesCard = ({ movieData }) => {
 
-  const { pathname } = useLocation();
-
-  const saveMovieHandler = () => {
-    console.log('Фильм сохранен');
-  }
-
-  const deleteMovieHandler = () => {
-    console.log('Фильм удален');
-  }
+  const location = useLocation();
+  const [isSaved, setIsSaved] = useState(movieData.inSaved);
 
   return(
     <div className="moviesCard">
@@ -31,11 +26,16 @@ const MoviesCard = ({ movieData }) => {
           {movieData.nameRU}
         </h2>
 
-            <MovieButton
-              onClickHandler={pathname === "/movies" ? saveMovieHandler : deleteMovieHandler}
-              className={ pathname === "/movies" ? "moviesCard__button-del" : "moviesCard__button-save"}
-            >
-            </MovieButton>
+        {location.pathname === "/saved-movies" &&
+              <button type="button" aria-label="удалить фильм" className="moviesCard__button-del">
+                  <img className="moviesCard__delete" alt="удалить" src={deleteFilmButton}/>
+              </button>}
+          {location.pathname === "/movies" &&
+              <button type="button" aria-label="сохранить" className={isSaved ? "moviesCard__button-save" : "moviesCard__button"}
+                  >
+                  {isSaved ? <img className="moviesCard__delete" alt="добавлено" src={saveButton}/> :
+                      <img className="moviesCard__add" alt="добавить" src={saveFilmButton}/>}
+              </button>}
 
         </div>
         <span className="moviesCard__duration">
